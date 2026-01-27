2e4548d111c91a9ac6fe675ff1a83736335f599d
Sindhuja Parthasarathy <sarathy.sindhu@gmail.com>
2026-01-27 12:04:38 +0100

    Updated the PersonRepository with functions

--- src/main/java/se/lexicon/g58todoapp/repo/PersonRepository.java
+++ src/main/java/se/lexicon/g58todoapp/repo/PersonRepository.java
@@ -3,10 +3,17 @@ package se.lexicon.g58todoapp.repo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import se.lexicon.g58todoapp.entity.Person;
 
+import java.util.List;
+
 public interface PersonRepository extends JpaRepository<Person, Long> {
 
+
+
     // TODO: Find person with a email
+    List<Person> findPersonByEmail(String email);
+
     // TODO: Is there a person with a specific email? - return boolean
+    boolean existsByEmail(String email);
 
 
 }
